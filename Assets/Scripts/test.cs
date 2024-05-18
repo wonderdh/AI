cd25c6f8b66025db21fc3340b6f46b1ea35f4587
wonderdh <wonderdh@naver.com>
2024-05-18 18:34:51 +0900

    formatting01

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : ScriptableObject
+{
+
+    [SerializeField]
+    public int x;
+    [SerializeField]
+    public int y;
+
+
+
+}
